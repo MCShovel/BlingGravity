7c21dfa30e3b3509ec9ca1beb67ed40d4da432c8
Minecraft User <rogerk@pvp-minecraft.com>
2016-08-18 13:38:44 -0500

    Added config options

--- src/main/java/com/sethbling/blinggravity/BlingGravity.java
+++ src/main/java/com/sethbling/blinggravity/BlingGravity.java
@@ -1,47 +1,19 @@
-/*
- * Decompiled with CFR 0_115.
- *
- * Could not load the following classes:
- *  org.bukkit.Location
- *  org.bukkit.Material
- *  org.bukkit.Server
- *  org.bukkit.World
- *  org.bukkit.block.Block
- *  org.bukkit.entity.Entity
- *  org.bukkit.entity.Player
- *  org.bukkit.event.EventHandler
- *  org.bukkit.event.EventPriority
- *  org.bukkit.event.Listener
- *  org.bukkit.event.entity.EntityDamageEvent
- *  org.bukkit.event.entity.EntityDamageEvent$DamageCause
- *  org.bukkit.plugin.Plugin
- *  org.bukkit.plugin.PluginManager
- *  org.bukkit.plugin.java.JavaPlugin
- *  org.bukkit.scheduler.BukkitTask
- *  org.bukkit.util.Vector
- */
 package com.sethbling.blinggravity;
 
 import com.sethbling.blinggravity.GravityTask;
 import java.util.HashMap;
-import java.util.List;
 import java.util.UUID;
-import java.util.logging.Logger;
 import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.Server;
 import org.bukkit.World;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageModifier;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitTask;
 import org.bukkit.util.Vector;
 
 public class BlingGravity
@@ -50,85 +22,99 @@ implements Listener {
     protected HashMap<UUID, Vector> velocities;
     protected HashMap<UUID, Location> positions;
     protected HashMap<UUID, Boolean> onGround;
+	private int tickUpdate;
+	private double fallDamage;
+	double gravity;
+	private boolean applyPlayers;
+	private boolean applyEntities;
 
     public void onEnable() {
         this.getLogger().info("BlingGravity has been loaded.");
-        new GravityTask(this).runTaskLater((Plugin)this, 1);
-        this.velocities = new HashMap();
-        this.onGround = new HashMap();
-        this.positions = new HashMap();
+        this.velocities = new HashMap<UUID, Vector>();
+        this.onGround = new HashMap<UUID, Boolean>();
+        this.positions = new HashMap<UUID, Location>();
+        
+        MainConfig cfg = new MainConfig(this);
+        cfg.load();
+        
+        this.gravity = cfg.getGravity();
+        this.tickUpdate = cfg.getTickUpdate();
+        this.applyPlayers = cfg.appliesToPlayers();
+        this.applyEntities = cfg.appliesToEntities();
+        		
+        this.getCommand("gravity").setExecutor(new CmdGravity(this));
         this.getServer().getPluginManager().registerEvents((Listener)this, (Plugin)this);
+        this.getServer().getScheduler().scheduleSyncRepeatingTask(this, new GravityTask(this), tickUpdate, tickUpdate);
     }
 
     public void onDisable() {
         this.getLogger().info("BlingGravity has been unloaded.");
     }
-
+    
     public void UpdateVelocities() {
         for (World world : this.getServer().getWorlds()) {
-            for (Entity e : world.getEntities()) {
-                Vector newv = e.getVelocity().clone();
-                UUID uuid = e.getUniqueId();
-                if (this.velocities.containsKey(uuid) && this.onGround.containsKey(uuid) && !e.isOnGround() && !e.isInsideVehicle()) {
-                    Vector oldv = this.velocities.get(uuid);
-                    if (!this.onGround.get(uuid).booleanValue()) {
-                        Vector d = oldv.clone();
-                        d.subtract(newv);
-                        double dy = d.getY();
-                        if (dy > 0.0 && (newv.getY() < -0.01 || newv.getY() > 0.01)) {
-                            boolean oldzchanged;
-                            boolean oldxchanged;
-                            Location loc = e.getLocation().clone();
-                            double gravity = 1.0;
-                            while (loc.getBlockY() >= 0) {
-                                Block block = loc.getBlock();
-                                if (block.getType() == Material.WOOL) {
-                                    if (block.getData() == 5) {
-                                        gravity = 0.2;
-                                    } else if (block.getData() == 14) {
-                                        gravity = 5.0;
-                                    } else if (block.getData() == 9) {
-                                        gravity = -0.2;
-                                    } else if (block.getData() == 2) {
-                                        gravity = -5.0;
-                                    }
-                                }
-                                if (block.getType() != Material.AIR) break;
-                                loc.setY(loc.getY() - 1.0);
-                            }
-                            newv.setY(oldv.getY() - dy * gravity);
-                            boolean newxchanged = newv.getX() < -0.001 || newv.getX() > 0.001;
-                            boolean bl = oldxchanged = oldv.getX() < -0.001 || oldv.getX() > 0.001;
-                            if (newxchanged && oldxchanged) {
-                                newv.setX(oldv.getX());
-                            }
-                            boolean newzchanged = newv.getZ() < -0.001 || newv.getZ() > 0.001;
-                            boolean bl2 = oldzchanged = oldv.getZ() < -0.001 || oldv.getZ() > 0.001;
-                            if (newzchanged && oldzchanged) {
-                                newv.setZ(oldv.getZ());
-                            }
-                            e.setVelocity(newv.clone());
-                        }
-                    } else if (e instanceof Player && this.positions.containsKey(uuid)) {
-                        Vector pos = e.getLocation().toVector();
-                        Vector oldpos = this.positions.get(uuid).toVector();
-                        Vector velocity = pos.subtract(oldpos);
-                        newv.setX(velocity.getX());
-                        newv.setZ(velocity.getZ());
+        	if (applyEntities) {
+        		for (Entity e : world.getEntities()) {
+        			if (!applyPlayers && e instanceof Player) { 
+        				continue; 
+    				}
+        			UpdateVelocitiesFor(e);
+        		}
+        	} else if (applyPlayers) {
+				for (Entity e : world.getPlayers()) {
+					UpdateVelocitiesFor(e);
+				}
+			}
+        }
+    }    	
+        	
+    public void UpdateVelocitiesFor(Entity e) {
+    	
+        Vector newv = e.getVelocity().clone();
+        UUID uuid = e.getUniqueId();
+        if (this.velocities.containsKey(uuid) && this.onGround.containsKey(uuid) && !e.isOnGround() && !e.isInsideVehicle()) {
+            Vector oldv = this.velocities.get(uuid);
+            if (!this.onGround.get(uuid).booleanValue()) {
+                Vector d = oldv.clone();
+                d.subtract(newv);
+                double dy = d.getY();
+                if (dy > 0.0 && (newv.getY() < -0.01 || newv.getY() > 0.01)) {
+                    boolean oldzchanged;
+                    boolean oldxchanged;
+                    newv.setY(oldv.getY() - dy * gravity);
+                    
+                    boolean newxchanged = newv.getX() < -0.001 || newv.getX() > 0.001;
+                    oldxchanged = oldv.getX() < -0.001 || oldv.getX() > 0.001;
+                    if (newxchanged && oldxchanged) {
+                        newv.setX(oldv.getX());
                     }
+                    
+                    boolean newzchanged = newv.getZ() < -0.001 || newv.getZ() > 0.001;
+                    oldzchanged = oldv.getZ() < -0.001 || oldv.getZ() > 0.001;
+                    if (newzchanged && oldzchanged) {
+                        newv.setZ(oldv.getZ());
+                    }
+                    
                     e.setVelocity(newv.clone());
                 }
-                this.velocities.put(uuid, newv.clone());
-                this.onGround.put(uuid, e.isOnGround());
-                this.positions.put(uuid, e.getLocation());
+            } else if (e instanceof Player && this.positions.containsKey(uuid)) {
+                Vector pos = e.getLocation().toVector();
+                Vector oldpos = this.positions.get(uuid).toVector();
+                Vector velocity = pos.subtract(oldpos);
+                newv.setX(velocity.getX());
+                newv.setZ(velocity.getZ());
             }
+            e.setVelocity(newv.clone());
         }
+        this.velocities.put(uuid, newv.clone());
+        this.onGround.put(uuid, e.isOnGround());
+        this.positions.put(uuid, e.getLocation());
     }
 
     @EventHandler(priority=EventPriority.HIGHEST)
     public void onEntityDamageEvent(EntityDamageEvent e) {
-        if (e.getCause() == EntityDamageEvent.DamageCause.FALL) {
-            e.setCancelled(true);
+     	if (e.getCause() == EntityDamageEvent.DamageCause.FALL) {
+        	e.setDamage(e.getFinalDamage() * this.gravity);
         }
     }
 }
--- src/main/java/com/sethbling/blinggravity/GravityTask.java
+++ src/main/java/com/sethbling/blinggravity/GravityTask.java
@@ -1,20 +1,9 @@
-/*
- * Decompiled with CFR 0_115.
- *
- * Could not load the following classes:
- *  org.bukkit.plugin.Plugin
- *  org.bukkit.scheduler.BukkitRunnable
- *  org.bukkit.scheduler.BukkitTask
- */
 package com.sethbling.blinggravity;
 
 import com.sethbling.blinggravity.BlingGravity;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scheduler.BukkitTask;
 
 public class GravityTask
-extends BukkitRunnable {
+implements Runnable {
     private final BlingGravity plugin;
 
     public GravityTask(BlingGravity plugin) {
@@ -23,6 +12,5 @@ extends BukkitRunnable {
 
     public void run() {
         this.plugin.UpdateVelocities();
-        new GravityTask(this.plugin).runTaskLater((Plugin)this.plugin, 1);
     }
 }
